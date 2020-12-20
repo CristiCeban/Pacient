9c1d8b779905f9e0ad59a9bba8ed892da30720c8
Alex <alexwarofcommand@gmail.com>
2020-12-20 15:36:25 +0200

    first commit

---
+++ src/main/java/com/pacienti/pacient/PacientApplication.java
@@ -0,0 +1,13 @@
+package com.pacienti.pacient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pacienti/pacient/PacientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacienti.pacient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
