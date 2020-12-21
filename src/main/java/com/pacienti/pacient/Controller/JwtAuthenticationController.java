70be7e1252b8760d8c69b1f29257296a3e5f9ae8
Alex <alexwarofcommand@gmail.com>
2020-12-21 16:11:38 +0200

    authetificate added

---
+++ src/main/java/com/pacienti/pacient/Controller/JwtAuthenticationController.java
@@ -0,0 +1,154 @@
+package com.pacienti.pacient.Controller;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.pacienti.pacient.Configuration.JwtTokenUtil;
+import com.pacienti.pacient.DTO.UserDto;
+import com.pacienti.pacient.Model.JwtRequest;
+import com.pacienti.pacient.Model.JwtResponse;
+import com.pacienti.pacient.Repository.UserRepository;
+import com.pacienti.pacient.Service.JwtUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping(path = "/api")
+public class JwtAuthenticationController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtTokenUtil jwtTokenUtil;
+
+	@Autowired
+	private JwtUserDetailsService userDetailsService;
+
+
+
+
+	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+	public ResponseEntity<?> generateAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
+			throws Exception {
+
+		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String token = jwtTokenUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public ResponseEntity<?> saveUser(@RequestBody UserDto user) throws Exception {
+		userDetailsService.save(user);
+		final UserDetails userdetails = new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+		final String token = jwtTokenUtil.generateToken(userdetails);
+		return ResponseEntity.ok(new JwtResponse(token));
+	}
+
+	private void authenticate(String username, String password) throws Exception {
+		Objects.requireNonNull(username);
+		Objects.requireNonNull(password);
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+		} catch (DisabledException e) {
+			throw new Exception("USER_DISABLED", e);
+		} catch (BadCredentialsException e) {
+			throw new Exception("INVALID_CREDENTIALS", e);
+		}
+	}
+
+	// @GetMapping(value = "/me")
+	// private ProfileResponse getProf(Authentication authenticate) {
+		
+	// 	UserDao user = userDetailsService.currentUser(authenticate);
+
+	// 	List<ProductDao> userProducts = productRepository.findAllByUserId(user);
+		
+	// 	List<LikedProduct> likedProducts = new ArrayList<>();
+
+    //     for (ProductDao product : userProducts) {
+    //         boolean isLiked = wishListRepository.existsByUserIdAndProductId(user.getId(), product.getId());
+    //         likedProducts.add(new LikedProduct(product,isLiked));
+    //     }
+
+	// 	return new ProfileResponse(user, likedProducts);
+
+	// }
+
+	// @GetMapping(value = "/profile/{id}")
+	// private ProfileResponse getProf(@PathVariable("id") Integer id,Authentication authentication) {
+		
+	// 	UserDao currentUser = userDetailsService.currentUser(authentication);
+
+	// 	UserDao user = userRepository.findById(id).orElseThrow();
+
+	// 	List<ProductDao> userProducts = productRepository.findAllByUserId(user);
+
+	// 	List<LikedProduct> likedProducts = new ArrayList<>();
+
+    //     for (ProductDao product : userProducts) {
+    //         boolean isLiked = wishListRepository.existsByUserIdAndProductId(currentUser.getId(), product.getId());
+    //         likedProducts.add(new LikedProduct(product,isLiked));
+    //     }
+
+	// 	return new ProfileResponse(user, likedProducts);
+	// }
+
+	// @PostMapping(value = "/me/update")
+	// private void updateProf(Authentication authentication,
+	// 		@RequestParam(required = false, name = "imagePath") MultipartFile fotka,
+	// 		@RequestParam(required = false, name = "name") String name,@RequestParam(required = false, name = "phone") String phone,@RequestParam(required = false, name = "username") String username) throws IOException {
+		
+		
+	// 	UserDao user = userDetailsService.currentUser(authentication);
+
+	// 	UserDto updatedUser = new UserDto();
+
+		
+	// 	if (fotka != null) {
+	// 		String imagePath = productController.saveImage(fotka);
+	// 		updatedUser.setImagePath(imagePath);
+	// 		System.out.println("Saved");
+	// 	}
+	// 	if(name != null){
+	// 		updatedUser.setName(name);
+	// 	}
+	// 	if (phone != null) {
+	// 		updatedUser.setPhone(phone);
+	// 	}
+	// 	if (username != null) {
+	// 		updatedUser.setUsername(username);;
+	// 	}
+	// 	userDetailsService.updateProf(user, updatedUser);
+	// }
+ }
---
+++ src/main/java/com/pacienti/pacient/Security/SecurityConfiguration.java
@@ -0,0 +1,138 @@
+package com.pacienti.pacient.Security;
+
+import java.util.Arrays;
+
+import com.pacienti.pacient.Configuration.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+	@Autowired
+	private UserDetailsService jwtUserDetailsService;
+
+	@Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		// configure AuthenticationManager so that it knows from where to load
+		// user for matching credentials
+		// Use BCryptPasswordEncoder
+		auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .cors().and().csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/").permitAll()
+            .antMatchers("/products/**").permitAll()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/api/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+            http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+            // .and()
+            // .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            // .antMatchers(HttpMethod.GET,"/all").permitAll()
+            // .antMatchers(HttpMethod.POST,"/add-user").permitAll()
+            // .and()
+            // .formLogin()
+            //     .defaultSuccessUrl("/", true)
+            //     .loginPage("/login").permitAll()
+            //     .and()
+            //     .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+
+
+    }
+
+    // @Bean
+	// @Override
+	// public UserDetailsService userDetailsService() {
+	// 	UserDetails user =
+	// 		 User.withDefaultPasswordEncoder()
+	// 			.username("user")
+	// 			.password("password")
+	// 			.roles("USER")
+	// 			.build();
+
+	// 	return new InMemoryUserDetailsManager(user);
+    // }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+//     @Bean
+// CorsConfigurationSource corsConfigurationSource() {
+//     CorsConfiguration configuration = new CorsConfiguration();
+//     configuration.setAllowedOrigins(Arrays.asList("*"));
+//     configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH"));
+//     configuration.setAllowCredentials(true);
+//     //the below three lines will add the relevant CORS response headers
+//     configuration.addAllowedOrigin("*");
+//     configuration.addAllowedHeader("*");
+//     configuration.addAllowedMethod("*");
+//     configuration.addAllowedOriginPattern();
+//     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//     source.registerCorsConfiguration("/**", configuration);
+//     return source;
+// }
+
+// @Bean
+// public CorsFilter corsFilter() {
+//     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//     CorsConfiguration config = new CorsConfiguration();
+//     config.setAllowedOrigins(Arrays.asList("*"));
+//     config.setAllowCredentials(true);
+//     config.addAllowedOrigin("*");
+//     config.addAllowedHeader("*");
+//     config.addAllowedMethod("OPTIONS");
+//     config.addAllowedMethod("GET");
+//     config.addAllowedMethod("POST");
+//     config.addAllowedMethod("PUT");
+//     config.addAllowedMethod("DELETE");
+//     source.registerCorsConfiguration("/**", config);
+//     return new CorsFilter(source);
+// }
+
+}
