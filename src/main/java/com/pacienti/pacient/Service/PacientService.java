d2d2ebc58db63da127b62b86a99da0a35c903c7e
Alex <alexwarofcommand@gmail.com>
2020-12-22 12:51:49 +0200

    something changed

--- src/main/java/com/pacienti/pacient/Service/PacientService.java
+++ src/main/java/com/pacienti/pacient/Service/PacientService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class PacientService {
     @Autowired
     private PacientRepository pacientRepository;
 
+    
     public PacientsDto getPacients(int page, int size, UserDao user) {
         Pageable pageRequest = PageRequest.of(page,size);
 
